99eb0372014c22f891408c0f3932fa961172de32
lkruwan <88529228+lkruwan@users.noreply.github.com>
2021-10-20 16:03:06 +0530

    Update video download.js

--- plugins/video download.js
+++ plugins/video download.js
@@ -11,7 +11,7 @@ const NO_RESULT = "*🌀can't Find Anything...*"
     amazone.addCommand({ pattern: 'video ?(.*)', fromMe: true, deleteCommand: false, desc:'video download',  deleteCommand: false}, async (message, match) => {
         const linkk = match[1]
         if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
-        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text);
+        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});
         await axios
           .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
           .then(async (response) => {
@@ -19,8 +19,27 @@ const NO_RESULT = "*🌀can't Find Anything...*"
               link,
             } = response.data.result
             const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
-            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text);
-            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {mimetype: Mimetype.mp4, ptt: false})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text , {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
+        })
+        .catch(
+          async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
+        )
+      },
+    )
+    amazone.addCommand({ pattern: 'video ?(.*)', fromMe: false, deleteCommand: false, desc:'video download',  deleteCommand: false}, async (message, match) => {
+        const linkk = match[1]
+        if (!linkk) return await message.client.sendMessage(message.jid,YT_NEED,MessageType.text)
+        await message.client.sendMessage(message.jid,DWLOAD_VID,MessageType.text , {quoted: message.data});
+        await axios
+          .get(`https://rei-api.herokuapp.com/api/dl/ytavv2?url=${linkk}`)
+          .then(async (response) => {
+            const {
+              link,
+            } = response.data.result
+            const videoBuffer = await axios.get(link, {responseType: 'arraybuffer'})
+            await message.client.sendMessage(message.jid,YTV_UP,MessageType.text , {quoted: message.data});
+            await message.client.sendMessage(message.jid,Buffer.from(videoBuffer.data), MessageType.video, {quoted: message.data ,mimetype: Mimetype.mp4, ptt: false})
         })
         .catch(
           async (err) => await message.client.sendMessage(message.jid,NO_RESULT,MessageType.text, {quoted: message.data}),
